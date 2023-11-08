405a38817664a3739922a013c9f0c1d51438d277
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-07 21:53:36 -0500

    fibonacci_recursive working

--- fibonacci_recursive.py
+++ fibonacci_recursive.py
@@ -1,22 +1,30 @@
-"""
-**Requirements**
+# Program to figure out the x(n) given an n for the fibonacci sequence
+# Must be done recursively, mathisfun is good reference for fibonnacci info
 
-* Given a term (n), determine the value of x(n).
-* In the *fibonacci_recursive.py* program, create a function called *fibonnaci*. 
-The function should take in an integer and return the value of x(n).
-* This problem must be solved using recursion. 
-"""
 
+# Learning from factorial recursion, either the ns or the xs in the formual will be the recursive part
+# if conditional so that when n is 0 or 1, it sets x to 0 or 1.  Seems making a recusive function keeps the variables from conflicting (x in this case)
 def fibonacci(n):
-    return none
 
-
-
-
-def getFibonacciValue(n):
-    x = (n - 1) + (n - 2)
+    if (n == 0):
+        x = 0
+    elif (n == 1):
+        x = 1
+    else:
+        x = fibonacci(n-1) + fibonacci(n-2)
     return x
 
-print(getFibonacciValue(9))
+user_input = input(f"Give me an integer, and I'll give you f(n) for the value in the Fibonnaci Sequence: ")
+int_to_nocci = int(user_input)
+ans = fibonacci(int_to_nocci)
+print(f"f(n) =  {ans}")
+
+
+
+# wrong, can't just drop the xs, or at least idk how to put it back in if I do yet, that seems mathy
+#def getFibonacciValue(n):
+#    x = (n - 1) + (n - 2)
+#    return x
+
 
 
