c7ae304e8b441098eeb271180d45c0eb14ecf2a4
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-07 22:21:55 -0500

    need to check on this

---
+++ fibonacci_linear.py
@@ -0,0 +1,20 @@
+# Program to determine f(n) for fibonacci given an n
+# This one idk about using, commented/docstring? out for now. Mathisfun provided a forumale for finding f(x) using golden ration
+# I see how it is working, but i don't understand why the golden ratio does what it does.
+
+"""
+import math
+def fibonacci(n):
+    x = 0
+    r = 1.61803398874
+    if (n == 0):
+        x = 0
+    elif (n == 1):
+        x = 1
+    else:
+        x = ((r**n)-((1-r)**n)) / math.sqrt(5)
+    
+    return x
+
+print(round(fibonacci(12)))
+"""
